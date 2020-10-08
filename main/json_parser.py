cf1190c62edcac4681bf0d25bc05cd698dceeb3f
Sanjay Balaji <sanjay.balaji@oracle.com>
2020-10-08 20:38:50 +0530

    changes

--- main/json_parser.py
+++ main/json_parser.py
@@ -1,17 +1,12 @@
 import json, requests, os
+# BASE URLS
+LIX_BASE_URL = "https://app.leanix.net/services"
+LIX_API_TOKEN_GEN_ENDPOINT = LIX_BASE_URL + "/mtm/v1/oauth2/token"
+LIX_SYNC_ENDPOINT = LIX_BASE_URL + "/integration-api/v1/synchronizationRuns"
 
-LIX_API_ENDPOINT = os.environ.get("LIX_API_ENDPOINT", "https://eu.leanix.net/services/integration-api")
-LIX_API_TOKEN = os.environ.get("LIX_API_TOKEN", "zQQEvgNLKmgNdLygWr9EcvtXUv8LOdYzySxgY9C7")
-TOKEN_GEN_ENDPOINT="https://app.leanix.net/services/mtm/v1/oauth2/token"
-
-# Class that uses BearerAuth
-class BearerAuth(requests.auth.AuthBase):
-    def __init__(self, token):
-        self.token = token
-    def __call__(self, r):
-        r.headers["Authorization"] = "Bearer " + self.token
-        r.headers["Content-Type"] = "application/json"
-        return r
+# OS ENV Vars
+LIX_API_TOKEN = os.environ.get("LIX_API_TOKEN")
+LIX_API_ENDPOINT = os.environ.get("LIX_API_ENDPOINT", LIX_BASE_URL + "/integration-api")
 
 def print_json(json_object):
     """
@@ -20,40 +15,49 @@ def print_json(json_object):
     print(json.dumps(json_object, indent=2)) 
 
 def getJWTToken():
-    data={
-        "grant_type":"client_credentials"
-    }
-    print("Generating JWT Token...")
-    post_response = requests.post(url=TOKEN_GEN_ENDPOINT, data=data, auth=('apitoken',LIX_API_TOKEN),)
-    if post_response.status_code == 200:
-        print("Everything looks good!")
-    else:
-        print("Something went wrong!")
-        return None
-    response_json = post_response.json()
-    print(response_json)
-    print("##DEBUG")
-    print("ACCESS_TOKEN: " + response_json["access_token"])
-    print("Done...")
-    return str(response_json["access_token"])
+    try:
+        payload={"grant_type":"client_credentials"}
+        print("Generating JWT Token...")
+        # do a basic auth to get the jwt token
+        response = requests.post(url=LIX_API_TOKEN_GEN_ENDPOINT, data=payload, auth=('apitoken', LIX_API_TOKEN))
+        response.raise_for_status() 
+        access_token = response.json()['access_token']
+        print("##DEBUG")
+        print("ACCESS_TOKEN: " + access_token)
+        print("Done...")
+    except Exception as error:
+        access_token = None
+        print ("Oops! An exception has occured:", error)
+        print ("Exception TYPE:", type(error))
+    finally:
+        return access_token
 
 def postDeployMetric(deploy_point):
     """
         Takes in a deploy_point of type dict/hashmap
         Calls the LEAN IX Synchronization Run 
     """
-    LIX_SYNC_ENDPOINT = LIX_API_ENDPOINT + "/v1/synchronizationRuns"
     try:
-        print("Calling " + str(LIX_SYNC_ENDPOINT))
         JWT_TOKEN = getJWTToken()
-        post_response = requests.post(url=LIX_SYNC_ENDPOINT, data=json.dumps(deploy_point), auth=BearerAuth(JWT_TOKEN))
-        print("Posted!, CODE: " + str(post_response.status_code))
-        if post_response.status_code == 200:
-            print("Everything looks good!")
-        response_json = post_response.json()
-        print(response_json)
+        auth_header = "Bearer " + JWT_TOKEN
+        headers = {
+            'Authorization': auth_header,
+            'Content-Type': 'application/json'
+        }
+        # submitting a sync run
+        print("Invoking " + LIX_SYNC_ENDPOINT)
+        json_payload = json.dumps(deploy_point, indent=2)
+        response = requests.post(url=LIX_SYNC_ENDPOINT, headers=headers, data=json_payload)
+        print("Posted!, STATUS CODE: " + str(response.status_code))
+        response.raise_for_status()
+        # some general status code handling 
+        if response.status_code in (200, 201, 202):
+            print("Looks like everything is fine...")
+        else:
+            print("Looks like something went wrong...")
+            raise requests.HTTPError()
+        print(response.json())
         print("Processing Done...")
-        #TODO add wait until it successfully posts, poll for sync run using ID
     except Exception as error:
         print ("Oops! An exception has occured:", error)
         print ("Exception TYPE:", type(error))
@@ -73,41 +77,31 @@ def testParser(json_object):
             check_suite = json_object['check_suite']
             check_suite_id = str(check_suite['id'])
             timestamp = str(check_suite['updated_at'])
-            conclusion = str(check_suite['conclusion'])
             print("#####################################################################")
-            print("INFO CI-ID#" + check_suite_id + ": This looks like a valid deployment | Deployment Timestamp: " + timestamp)
+            print("INFO CI-ID#" + check_suite_id + ": This looks like a successful deployment | Deployment Timestamp: " + timestamp)
             # aggregating the POST requests into data points
             deploy_point = {
-                    "connectorId": "leanix-deployment-connector",
-                    "connectorType": "leanix",
-                    "connectorVersion": "1.0",
-                    "processingDirection": "inbound",
-                    "processingMode": "partial",
-                    "lxVersion": "1.0.0",
-                    "description": "DeploymentFrequency Metric",
-                    "content": [
-                        {
-                            "id": "Deployment Frequency",
-                            "type": "microservice",
-                            "data": {
-                                "name": json.dumps(repo_name),
-                                "deploymentTime": json.dumps(timestamp)
-                            }
+                "connectorId": "leanix-deployment-connector",
+                "connectorType": "leanix",
+                "connectorVersion": "1.0.0",
+                "processingDirection": "inbound",
+                "processingMode": "partial",
+                "lxVersion": "1.0.0",
+                "description": "DeploymentFrequency Metric",
+                "content": [
+                    {
+                        "id": "Deployment Frequency",
+                        "type": "microservice",
+                        "data": {
+                            "name": repo_name,
+                            "deploymentTime": timestamp
                         }
-                    ]
-                }
+                    }
+                ]
+            }
+            # print json and process it
             print_json(deploy_point)
-            postDeployMetric(deploy_point)
-            # aggregating that into a file for now
-            # with open('./data/aggregation.json', 'r+') as file:
-            #     print("Aggregating data...")
-            #     aggregated_json = json.load(file)
-            #     file.seek(0)
-            #     # appending the deploy point to the content array
-            #     aggregated_json['content'].append(deploy_point)
-            #     # sorting keys to maintain order
-            #     json.dump(aggregated_json, file, indent=2, sort_keys=True)         
-            #     print("Done...")       
+            postDeployMetric(deploy_point)      
             print("#####################################################################")
         else:
             print("#####################################################################")
