861507c1718929af453c17aa1dd33ace0ee1f0e4
Sanjay Balaji <sanjay.balaji@oracle.com>
2020-10-09 06:38:29 +0530

    added start sync run

--- main/json_parser.py
+++ main/json_parser.py
@@ -31,14 +31,14 @@ def getJWTToken():
     finally:
         return access_token
 
-def postDeployMetric(deploy_point):
+def createSyncRun(deploy_point, auth_token):
     """
-        Takes in a deploy_point of type dict/hashmap
-        Calls the LEAN IX Synchronization Run 
+        Creates a new sync run using the LDIF present in deploy_point dict
+        Returns sync_id of the new sync run
     """
+    sync_id = None
     try:
-        JWT_TOKEN = getJWTToken()
-        auth_header = "Bearer " + JWT_TOKEN
+        auth_header = "Bearer " + auth_token
         headers = {
             'Authorization': auth_header,
             'Content-Type': 'application/json'
@@ -50,11 +50,34 @@ def postDeployMetric(deploy_point):
         print("Posted!, STATUS CODE: " + str(response.status_code))
         response.raise_for_status()
         print(response.json())
+        sync_id = response.json()['id']
         print("Processing Done...")
     except Exception as error:
         print ("Oops! An exception has occured:", error)
         print ("Exception TYPE:", type(error))
+    finally:
+        return sync_id
 
+def startSyncRun(sync_id, auth_token):
+    """
+        Initiates the sync run for a given sync_id
+    """
+    try:
+        auth_header = "Bearer " + auth_token
+        headers = {
+            'Authorization': auth_header,
+            'Content-Type': 'application/json'
+        }
+        LIX_START_SYNC_ENDPOINT = LIX_SYNC_ENDPOINT + "/" + sync_id + "/start"
+        print("Invoking " + LIX_START_SYNC_ENDPOINT )
+        response = requests.post(url=LIX_SYNC_ENDPOINT, headers=headers)
+        print("Posted!, STATUS CODE: " + str(response.status_code))
+        response.raise_for_status()
+        print(response.json())
+        print("Processing Done...")
+    except Exception as error:
+        print ("Oops! An exception has occured:", error)
+        print ("Exception TYPE:", type(error))
 
 def testParser(json_object):
     """
@@ -94,7 +117,9 @@ def testParser(json_object):
             }
             # print json and process it
             print_json(deploy_point)
-            postDeployMetric(deploy_point)      
+            auth_token = getJWTToken()
+            sync_id = createSyncRun(deploy_point, auth_token)
+            startSyncRun(sync_id, auth_token)
             print("#####################################################################")
         else:
             print("#####################################################################")
